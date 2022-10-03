bb63f45b8f2ae6a47bd3b3eba7a3d613176bafc7
Hiteshsukhwani29 <hiteshsukhwani29@gmail.com>
2022-10-03 20:39:58 +0530

    Create Searchbar

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+import Searchbar from "../../components/Searchbar";
+import Homebg from "../../assets/homebg.png";
+
+function Index() {
+  return (
+    <div className="home">
+      <Searchbar />
+    </div>
+  );
+}
+
+export default Index;
